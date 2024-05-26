18d54e6de5f6f9111940715797b2e8f93a0a8449
millenniumSonule <anshumansonuley@gmail.com>
2024-05-26 20:50:10 +0530

    .

--- src/client/Homepage.jsx
+++ src/client/Homepage.jsx
@@ -1,20 +1,17 @@
-// client/Homepage.js
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProducts } from './Redux/apiCallDummy';
+import './Homepage.css'; // Import the CSS file for styling
 
 const Homepage = () => {
   const dispatch = useDispatch();
-  const products = useSelector((state) => state.apiCallDummy.data); // Accessing data property
+  const products = useSelector((state) => state.apiCallDummy.data);
   const status = useSelector((state) => state.apiCallDummy.status);
   const error = useSelector((state) => state.apiCallDummy.error);
 
-
   useEffect(() => {
-    
     dispatch(fetchProducts());
-    
-  },[dispatch]);
+  }, [dispatch]);
 
   if (status === 'loading') {
     return <div>Loading...</div>;
@@ -25,18 +22,19 @@ const Homepage = () => {
   }
 
   return (
-    <div>
-        <h1>Products</h1>
-
-        {products.map((item,index)=>(
-            <div key={index}>
-              {index}
-              </div>
+    <div className="container">
+      <h1 className="title">Products</h1>
+      <div className="products-grid">
+        {products.map((product, index) => (
+          <div className="product-card" key={index}>
+            <img src={product.thumbnail} alt={product.title} className="product-image" />
+            <div className="product-index">$ {product.price}</div>
+            <div className="product-title">{product.title}</div>
+          </div>
         ))}
-
+      </div>
     </div>
-);
-
+  );
 };
 
 export default Homepage;
